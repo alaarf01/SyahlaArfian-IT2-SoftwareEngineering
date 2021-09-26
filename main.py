84fe282a84a9bd98a4cc99ef3fc57aca925a9405
alaarf01 <91398605+alaarf01@users.noreply.github.com>
2021-09-26 09:00:50 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,57 @@
+# Variabel
+
+a = 2 #integer / bilangan bulat
+hewan = "Gajah" # String
+#untuk yang dasarnya tidak ada yang namanya Char
+b = 'A'  # String
+c = True # Boolean
+d = False # Boolean
+e = 2.0 # Float
+f = [] # List / Array
+g = {} # Dictionary
+h = () # Tuple
+
+# Print
+
+print(hewan)
+print("Kuda")
+print(2)
+print(2+3)
+print(True)
+
+# Print 2
+print("Nama hewannya adalah: " + hewan)
+print("Nama hewannya adalah: {}".format(hewan))
+print("Nama : {}, Tempat Tinggal : {}, Umur : {}".format("Ala", "Cianjur", "21"))
+
+Nama_Siswa = ["Lili", "Ala", "Gia", "Desy", "JShotaro"]
+
+print(Nama_Siswa[0])
+print(Nama_Siswa[1])
+print(Nama_Siswa[2])
+print(Nama_Siswa[3])
+
+Nama_Siswa
+
+List_1 = ["Shotaro"] * 5
+List_1
+
+
+Nama_Siswa[0:2]
+
+Nama_Siswa[-2]
+
+Nama_Siswa[-3:]
+
+Nama_Siswa.append(1)
+Nama_Siswa
+
+Nama = "Shotaro"
+Tempat = "Tokyo"
+Umur = 21
+print("Nama : {}, Tempat Tinggal : {}, Umur : {}".format(Nama, Tempat, Umur))
+
+
+list_nct = ["Shotaro", "Haechan", "Jaemin", "Taeil"]
+list_nct[-2]
+
